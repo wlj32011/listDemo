9b829ef9cd99730280cac07d2bd047475f12af36
wanglj <372184603@qq.com>
2016-07-21 19:39:26 +0800

    解决webview无法加载的问题

--- app/src/main/java/cn/boc/listdemo/Html5Activity.java
+++ app/src/main/java/cn/boc/listdemo/Html5Activity.java
@@ -3,7 +3,13 @@ package cn.boc.listdemo;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import static cn.boc.listdemo.R.id.webview;
 
 public class Html5Activity extends Activity {
 
@@ -12,11 +18,25 @@ public class Html5Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_html5);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        final WebView webView = (WebView) findViewById(webview);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+
+                return true;
+            }
+        });
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
         //加载web地址
-        webView.loadUrl("http://m.sui.taobao.org/demos/");
+//        webView.loadUrl("http://m.sui.taobao.org/demos/");
 //        webView.loadUrl("http://www.baidu.com");
         //加载assets目录网页
-//        webView.loadUrl("file:///android_asset/index.html");
+        webView.loadUrl("file:///android_asset/index.html");
+
+
     }
 }
